1b3e6675b9c4ba61d5e2e404627392809092367a
Timothy Lee <timmy.ccl@gmail.com>
2016-08-30 09:56:28 +0800

    add tag column filter

--- main.js
+++ main.js
@@ -35,6 +35,24 @@ $(function() {
         },
         "targets": 5
       }
-    ]
+    ],
+    initComplete: function () {
+      var column = this.api().column(4);
+      var select = $('<select><option value=""></option></select>')
+        .appendTo( $(column.footer()).empty() )
+        .on( 'change', function () {
+          var val = $.fn.dataTable.util.escapeRegex(
+            $(this).val()
+          );
+
+          column
+            .search( val ? '^'+val+'$' : '', true, false )
+            .draw();
+        } );
+
+      column.data().unique().sort().each( function ( d, j ) {
+        select.append( '<option value="'+d+'">'+d+'</option>' )
+      } );
+    }
   });
 });
